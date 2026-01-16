af246f24d26386d59c1a915a8c3b0ba476ad4cc0
Temor Sam <temor@minerapp.dev>
2026-01-16 14:21:29 +0430

    add api route for telegram bot

---
+++ miner-miniapp/app/api/route.js
@@ -0,0 +1,7 @@
+export async function GET(request) {
+  return new Response("Bot is connected and ready!");
+}
+
+export async function POST(request) {
+  return new Response("Bot is connected and ready!");
+}
